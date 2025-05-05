62ccf3c932703bec9a6899675314c834b322439d
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-05-06 04:18:09 +0530

    add till onceNonce

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,12 +3,37 @@ use handlers_utils::handlers_utils;
 /// Registers the `ao` module with Lua, initializing the `ao` table with fields and functions.
 #[mlua::lua_module]
 pub fn handlers(lua: &Lua) -> LuaResult<LuaTable> {
-    // load the ao module
+    // load the handlers module
     let require: LuaFunction = lua.globals().get("require")?;
     let handlers_lua: LuaTable = require.call(".handlers")?;
     // Set static fields
     handlers_lua.set("_version", "0.0.5")?;
     handlers_lua.set("utils", handlers_utils(lua)?)?;
+
+    match lua.globals().get::<LuaTable>("Handlers") {
+        Ok(handlers_table) => {
+            handlers_table.set(
+                "list",
+                handlers_table
+                    .get::<LuaTable>("list")
+                    .unwrap_or_else(|_| lua.create_table().unwrap()),
+            )?;
+            handlers_table.set(
+                "coroutines",
+                handlers_table
+                    .get::<LuaTable>("coroutines")
+                    .unwrap_or_else(|_| lua.create_table().unwrap()),
+            )?;
+        }
+        Err(_) => {
+            let new_handlers = lua.create_table()?;
+            new_handlers.set("list", lua.create_table()?)?;
+            new_handlers.set("coroutines", lua.create_table()?)?;
+            lua.globals().set("Handlers", new_handlers)?;
+        }
+    }
+
+    handlers_lua.set("onceNonce", 0)?;
 
     Ok(handlers_lua)
 }

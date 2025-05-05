b23fc3aed6cc31c5fe6a087548b0e2e9b93b9fee
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-05-06 03:48:45 +0530

    add basic template

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,1 +1,14 @@
 use super::*;
+
+/// Registers the `ao` module with Lua, initializing the `ao` table with fields and functions.
+#[mlua::lua_module]
+pub fn handlers(lua: &Lua) -> LuaResult<LuaTable> {
+    // load the ao module
+    let require: LuaFunction = lua.globals().get("require")?;
+    let handlers_lua: LuaTable = require.call(".handlers")?;
+
+    // Set static fields
+    handlers_lua.set("_version", "0.0.5")?;
+
+    Ok(handlers_lua)
+}
